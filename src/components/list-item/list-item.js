8832f60e5ffdee946452d4cf60506dcb8c59fc39
David Gabrielian <davidgabrielofficial@gamil.com>
2020-08-01 16:11:47 +0400

    '1.0.1'

---
+++ src/components/list-item/list-item.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class ListItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1> hello from ListItem</h1>
+            </div>
+        )
+    }
+}
+export default ListItem
---
+++ src/components/list/list.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ListItem from "../list-item/list-item";
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <h1> hello from List</h1>
+        <ListItem />
+      </div>
+    );
+  }
+}
+
+export default List;
